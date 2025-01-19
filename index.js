4e6b24a0251fe3423755da1f5ab155dd24d7b187
BRIGHT OMOLAYO <molayobright@gmail.com>
2025-01-19 06:06:45 +0100

    19th jan

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
+// const http = require('http')
+// const url = require('url')
+// const fs = require('fs')
 
-http.createServer(function (req, res) {
-  // eslint-disable-next-line n/no-deprecated-api
-  const q = url.parse(req.url, true)
-  const filename = '.' + q.pathname
-  fs.readFile(filename, function (err, data) {
+// http.createServer(function (req, res) {
+//   // eslint-disable-next-line n/no-deprecated-api
+//   const q = url.parse(req.url, true)
+//   const filename = '.' + q.pathname
+//   fs.readFile(filename, function (err, data) {
+//     if (err) {
+//       res.writeHead(404, { 'Content-Type': 'text/html' })
+//       return res.end('404 Not Found')
+//     }
+//     res.writeHead(200, { 'Content-Type': 'text/html' })
+//     res.write(data)
+//     return res.end()
+//   })
+// }).listen(8080)
+
+const express = require('express')
+const app = express()
+const path = require('path')
+console.log(__dirname)
+
+app.get('/:filename', (req, res) => {
+  const fileName = req.params.filename
+  const filePath = path.join(__dirname, 'public', fileName)
+  res.sendFile(filePath, (err) => {
     if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' })
-      return res.end('404 Not Found')
+      res.status(404).send('File not found!')
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write(data)
-    return res.end()
   })
-}).listen(8080)
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`)
+})
