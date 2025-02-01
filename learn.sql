d5afa98fd0910b61b729f1494ced8a95d1885594
BRIGHT OMOLAYO <molayobright@gmail.com>
2025-02-01 06:38:58 +0100

    feb 1

---
+++ learn.sql
@@ -0,0 +1,8 @@
+
+SELECT emp_no as "Employee #", birth_date as "Birthday", first_name AS "First Name" FROM employees;
+SELECT * FROM titles
+SELECT CONCAT(emp_no, ' works as a ',title) as "Employee Title" FROM titles
+SELECT CONCAT( '   ', first_name, '   ',last_name) as "Employee Full_Names" FROM employees
+SELECT emp_no, CONCAT(first_name, '   ', last_name ) as "Employee Full_names" FROM employees;
+SELECT SUM(salary) FROM salaries;
+I want to select employee with the name Mayumi schueller
