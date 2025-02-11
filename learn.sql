234a7da75194829438a3e086e7d70f984f975a9d
BRIGHT OMOLAYO <molayobright@gmail.com>
2025-02-11 14:58:49 +0100

    sql

--- learn.sql
+++ learn.sql
@@ -5,4 +5,5 @@ SELECT CONCAT(emp_no, ' works as a ',title) as "Employee Title" FROM titles
 SELECT CONCAT( '   ', first_name, '   ',last_name) as "Employee Full_Names" FROM employees
 SELECT emp_no, CONCAT(first_name, '   ', last_name ) as "Employee Full_names" FROM employees;
 SELECT SUM(salary) FROM salaries;
-I want to select employee with the name Mayumi schueller
+-- I want to select employee with the name Mayumi schueller
+SELECT last_name, first_name from public.employees WHERE last_name = 'Schueller' AND first_name = 'Mayumi';
